f72fc5ea89e24ed6908b2de5770762140ad1f81e
Zeronetsec <senxhunter996@gmail.com>
2026-04-13 08:11:38 +0700

    modified: core/collections/muxly/install.sh

--- core/collections/muxly/install.sh
+++ core/collections/muxly/install.sh
@@ -5,6 +5,16 @@ if [[ ! -d "${GMOPT}" ]]; then
     command mkdir -pv "${GMOPT}"
 fi
 
+if [[ ! -d "${GMTMP}" ]]; then
+    command mkdir -p "${GMTMP}"
+fi
+
+command rm -fv "${GMTMP}/muxly_config.conf"
+if [[ -f "${GMOPT}/muxly/config/config.conf" ]]; then
+    command cat "${GMOPT}/muxly/config/config.conf" \
+        > "${GMTMP}/muxly_config.conf"
+fi
+
 if [[ -d "${GMOPT}/muxly" ]]; then
     command rm -rfv "${GMOPT}/muxly"
 fi
@@ -16,6 +26,9 @@ fi
 command git clone --depth 1 \
     'https://github.com/Zeronetsec/Muxly' \
     "${GMOPT}/muxly"
+
+command cat "${GMTMP}/muxly_config.conf" \
+    > "${GMOPT}/muxly/config/config.conf"
 
 command chmod +x -Rv "${GMOPT}/muxly"
 command ln -sfv \
